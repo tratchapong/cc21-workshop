2ae01177a7eedf1d5e91e9632483332a9d953179
tratchapong <tratchapong@gmail.com>
2025-09-23 15:45:36 +0700

    CartSummary mock UI

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -3,10 +3,24 @@ import CartItem from './CartItem'
 
 function CartSummary() {
   return (
-    <div className='max-md:hidden '>
+    <div className='w-1/3 max-md:hidden p-2 text-amber-900 '>
+      <h2 className='text-2xl py-2 text-slate-600 '>Cart Items : </h2>
       <CartItem />
       <CartItem />
       <CartItem />
+      <div className="divider opacity-50">Total</div>
+      <div className="flex justify-between px-1">
+        <div className="font-bold">Total</div>
+        <p>9,999.99</p>
+      </div>
+      <div className="flex justify-between px-1">
+        <div className="font-bold">VAT</div>
+        <p>99.99</p>
+      </div>
+      <div className="flex justify-between px-1">
+        <div className="font-bold">Grand Total</div>
+        <p>99.99</p>
+      </div>
     </div>
   )
 }
