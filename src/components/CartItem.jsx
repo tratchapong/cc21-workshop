cdd826122161d3d90495f1141369cc277ac3bfb6
tratchapong <tratchapong@gmail.com>
2025-09-23 15:57:44 +0700

    CartItem mock UI

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function CartItem() {
   return (
-    <div>CartItem</div>
+    <div className='flex justify-between items-center p-1 rounded bg-lime-50'>
+      <p>Product Title</p>
+      <div className="flex">
+        <p className='border w-5 h-5 flex justify-center items-center cursor-pointer'>-</p>
+        <p className='border w-5 h-5 flex justify-center items-center cursor-pointer text-white bg-amber-500'>1</p>
+        <p className='border w-5 h-5 flex justify-center items-center cursor-pointer'>+</p>
+      </div>
+      <p>9,999,.99</p>
+    </div>
   )
 }
 
--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -3,7 +3,8 @@ import CartItem from './CartItem'
 
 function CartSummary() {
   return (
-    <div className='w-1/3 max-md:hidden p-2 text-amber-900 '>
+    // <div className='w-1/3 max-md:hidden p-2 text-amber-900 '>
+    <div className='w-1/3 p-2 text-amber-900 '>
       <h2 className='text-2xl py-2 text-slate-600 '>Cart Items : </h2>
       <CartItem />
       <CartItem />
