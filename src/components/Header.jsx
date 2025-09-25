31898fdc6e1e1524ca503669662e784104f86fab
tratchapong <tratchapong@gmail.com>
2025-09-25 14:54:59 +0700

    navigate to landing-page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CartCount from './CartCount'
+import { useNavigate } from 'react-router'
 
 function Header(props) {
   const {itemCount} = props
+  const navigate = useNavigate()
   return (
     <div className='h-1/12 flex justify-between items-center px-4 border-b-3 border-pink-200'>
-      <div className='h-4/5 flex items-end gap-2'>
+      <div className='h-4/5 flex items-end gap-2 cursor-pointer'
+      onClick={()=>navigate('/')}>
         <img className='h-full'
          src="https://www.svgrepo.com/show/490951/shop.svg" alt="Shop" />
          <h3 className='text-xl font-bold text-amber-700'>CC21 Shop</h3>
